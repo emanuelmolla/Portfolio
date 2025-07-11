4b435dc79278a6855cddd372962ee07cea85c230
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-11 11:53:11 -0700

    Setup express working environment

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`All good server is runing on port: ${PORT}`);
+});
