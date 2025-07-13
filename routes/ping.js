f0d3ab614dd18705828a4df16a4c8c7ce4f794fe
Emanuel-DevX <jefymoxi@gmail.com>
2025-07-13 15:20:30 -0700

    Fix typo

--- routes/ping.js
+++ routes/ping.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const router = express.Router(
-  router.get("/", (req, res) => {
-    res.status(200).json({ message: "Server is up!" });
-  })
-);
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is up!" });
+});
 
 module.exports = router;
