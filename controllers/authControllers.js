27c9a8eb2195970464dbb8f23f4f488d3b45b7f2
Emanuel-DevX <jefymoxi@gmail.com>
2025-08-30 19:39:29 -0700

    Extend admin token expiry date to 7 days

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 
 const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d", // token valid for 1 day
+    expiresIn: "7d", // token valid for 7 days
   });
 };
 
