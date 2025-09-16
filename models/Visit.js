04c9d9d43ba97bdbe402e83982f6f6536a8ad50e
Emanuel-DevX <jefymoxi@gmail.com>
2025-09-15 17:37:45 -0700

    feat(visit): add country and city fields to Visit model

--- models/Visit.js
+++ models/Visit.js
@@ -5,6 +5,8 @@ const visitSchema = new mongoose.Schema(
     ip: { type: String }, // optional: track visitor IP
     userAgent: { type: String }, // optional: browser/device info
     date: { type: Date, default: Date.now },
+    country: String,
+    city: String,
   },
   { timestamps: true }
 );
