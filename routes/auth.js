40c2dc922949142d3b0335edf67ee1432a79ad82
Emanuel-DevX <jefymoxi@gmail.com>
2025-09-15 18:33:01 -0700

    Setup verify token route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { login } = require("../controllers/authControllers");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/login", login);
+router.get("/verify", authMiddleware, (req, res) => {
+  return res.json({ valid: true, user: req.user });
+});
 
 module.exports = router;
